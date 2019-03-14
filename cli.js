fb936cbef79d1583d335eee018ed866d858222fb
jamsinclair <jamsinclairnz@gmail.com>
2019-03-14 22:22:26 +0900

    Fix spaces

--- cli.js
+++ cli.js
@@ -5,16 +5,16 @@ const {startServer} = require('.')
 
 const cli = meow(
   `
-	Usage
-	  $ web-ext-reload path/to/watch
+  Usage
+    $ web-ext-reload path/to/watch
 
-	Options
-	  --ignoredPaths, -i File/Glob of paths to ignore (Default: node_modules)
-	  --port,         -p Port number to start the reload server on (Default: 3030)
+  Options
+    --ignoredPaths, -i File/Glob of paths to ignore (Default: node_modules)
+    --port,         -p Port number to start the reload server on (Default: 3030)
 
-	Examples
+  Examples
     $ web-ext-reload ./
-	  $ web-ext-reload ./dist -p 9000 -i ./dist/config
+    $ web-ext-reload ./dist -p 9000 -i ./dist/config
 `,
   {
     flags: {
